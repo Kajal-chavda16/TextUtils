6997b6e2b58a2aa6788586dbf8a0e1ad312653a8
Kajal-chavda16 <kajalchavda416@gmail.com>
2024-06-20 21:38:14 +0530

    TextUtils app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,73 @@
+import { useState } from 'react';
 import './App.css';
 import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
+  const [mode , setMode] = useState('light');
+  const [alert , setAlert] = useState(null);
+
+  const showAlert = (message,type) => {
+      setAlert({
+        msg : message ,
+        type : type
+      });
+      setTimeout(() => {
+        setAlert(null);
+      },1500);
+  };
+
+  const removeBodyClasses = () => {
+      document.body.classList.remove('bg-primary');
+      document.body.classList.remove('bg-danger');
+      document.body.classList.remove('bg-warning');
+      document.body.classList.remove('bg-success');
+      document.body.classList.remove('bg-light');
+      document.body.classList.remove('bg-dark');
+  };
+
+  const changeColor = (cls) => {
+    removeBodyClasses();
+    document.body.classList.add('bg-'+cls);
+  };
+
+const toggleMode = () => {
+  // removeBodyClasses();
+  // document.body.classList.add('bg-'+cls);
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#212529';
+      document.body.style.color = 'white';
+      showAlert("Dark mode has been set" , "success");
+     
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      document.body.style.color = 'black';
+      showAlert("Light mode has been set" , "success");
+    }
+  };
+
   return (
     <>
-      <Navbar title="TextUtils" />
+    <Router>
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} changeColor={changeColor} />
+      <Alert alert={alert} />
       <div className="container my-5" >
-          <TextForm heading="Enter your text here to analyze" /> 
-          {/* <About/> */}
+        <Routes>
+            <Route exact path="/about" element={<About />} />
+            <Route exact path="/" element={ <TextForm heading="Enter your text here to analyze" mode={mode} showAlert={showAlert} /> } />
+        </Routes>  
       </div>
+    </Router>
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,16 +6,19 @@ export default function TextForm(props) {
         // console.log("Uppercase was clicked " + text);
         let newText = text.toUpperCase();
         setText(newText);
+        props.showAlert("Changed to Uppercase !" , "success");
     }
 
     const handleLoClick = () => {
         let newText = text.toLowerCase();
         setText(newText);
+        props.showAlert("Changed to Lowercase !" , "success");
     }
 
     const handleClearClick = () => {
         let newText = '';
         setText(newText);
+        props.showAlert("Cleared text !" , "success");
     }
 
     const handleOnChange = (event) => {
@@ -31,18 +34,18 @@ export default function TextForm(props) {
             <h3>{props.heading}</h3>
             <br />
             <div className="mb-3">
-                <textarea className="form-control" value={text} onChange={handleOnChange} id="my-box" rows="8"></textarea>
+                <textarea className="form-control" value={text}  style={{backgroundColor : `${props.mode === 'light'?'white':'#212529'}` , color:`${props.mode === 'light'?'black':'white'}`}} id="my-box" onChange={handleOnChange} rows="8"></textarea>
             </div>
-            <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
-            <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
-            <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear Text</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={handleUpClick}>Convert to Uppercase</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={handleLoClick}>Convert to Lowercase</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={handleClearClick}>Clear Text</button>
         </div>
         <div className="container my-4">
             <h4>Your Text Summary</h4>
-            <p>{text.split(" ").length} words , {text.length} characters</p>
-            <p>{0.008 * text.split(" ").length} Minutes Read </p>
+            <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words , {text.length} characters</p>
+            <p>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes Read </p>
             <h5>Preview</h5>
-            <p>{text}</p>
+            <p>{text.length>0?text:"Nothing to preview !"}</p>
         </div>
     </>
   );
